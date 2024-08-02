68e59c6e2570ef8d6867d1f2b66e6aaa65ff55aa
Florence-nyokabi <florence@DESKTOP-I8K4KUQ>
2024-08-02 16:08:16 +0300

    Made the NavBar Responsive

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,44 +3,45 @@ import './style.css';
 import { MdOutlineFileDownload } from "react-icons/md";
 
 function About() {
-    return(
-    <div className="about" id="about-section">
-        <br/>
-        <br/>
-        <div className="home-divs">
-            <div className="home-div-one">
-                <span className="span-dot"></span>
-                {/* <br/> */}
-                <h1 className="name"> Florence Wangui</h1>
-                <br/>
-                <h1 className="title">
-                    Software Engineer & 
-                    <br/>
-                    QA Expert
-                </h1>
-                <p className="about-me">
-                While I may be at the beginning of my journey, my enthusiasm for backend and 
-                Android development knows no bounds. I thrive on the excitement of learning 
-                and implementing the latest trends and technologies to craft seamless user experiences.
-                </p>
-                <div className="resume-button">
-                    <a href="/public/Florence Wangui - Curriculum Vitae.pdf" download>
-                    <button>Resume  < MdOutlineFileDownload /></button>
-                    </a>
+    return (
+        <div className="about" id="about-section">
+            <br />
+            <br />
+            <div className="home-divs">
+                <div className="home-div-one">
+                    <span className="span-dot"></span>
+                    <h1 className="name"> Florence Wangui</h1>
+                    <br />
+                    <h1 className="title">
+                        Software Engineer & 
+                        <br />
+                        QA Expert
+                    </h1>
+                    <p className="about-me">
+                        While I may be at the beginning of my journey, my enthusiasm for backend and 
+                        Android development knows no bounds. I thrive on the excitement of learning 
+                        and implementing the latest trends and technologies to craft seamless user experiences.
+                    </p>
+                    <div className="resume-button">
+                        <a href="/public/documents/resume.pdf" download>
+                            <button>
+                                Resume <MdOutlineFileDownload />
+                            </button>
+                        </a>
+                    </div>
+                </div>
+                <div className="home-div-two">
+                    <div className="child-background" data-testid="child-background">
+                    </div>
+                </div>
+                <div className="dot-division">
+                    <span className="span-dot-two"></span>
                 </div>
             </div>
-            <div className="home-div-two">
-                <div className="child-background" data-testid="child-background">
-                </div>
-            </div>
-            <div className="dot-division">
-                <span className="span-dot-two"></span>
-            </div>
+            <br />
+            <br />
         </div>
-        <br/>
-        <br/>
-    </div>
-);
-    
+    );
 }
+
 export default About;
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -7,9 +7,9 @@ const Navbar = () => {
   return (
     <div className="Navbar">
       <span className="logo">
-      <img src="./images/logo.jpg" alt="" />
+        <img src="./images/logo.jpg" alt="Logo" />
       </span>
-      <div className={`nav-items ${isOpen && "open"}`} data-testid="nav-items"> 
+      <div className={`nav-items ${isOpen && "open"}`} data-testid="nav-items">
         <a href="#about-section">About</a>
         <a href="#my-skills">Skills </a>
         <a href="#my-projects">Projects</a>
